58e147fc93bb27b0ed0ab77172127168caad7aad
deepakkhungar <deepakkhungar@yahoo.com>
2017-05-15 13:08:25 -0400

    AngularJs 1.x

---
+++ AngularJs/Scripts/app/controllers/participantController.js
@@ -0,0 +1,116 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('AngularLab1')
+        .controller('participantController', participantController);
+
+	participantController.$inject = ['$scope', '$filter', 'participantFactory'];
+
+	function participantController($scope, $filter, participantFactory) {
+        $scope.title = 'participantController';
+
+		$scope.Participants = [];
+
+		$scope.ParticipantInfo = {
+			"PID": "",
+			"Name": "",
+			"Gender": "",
+			"Age": "",
+			"Color" : ""
+		};
+
+		$scope.GetTotalParticpants = function () {
+			alert(participantFactory.GetTotalParticipants($scope.Participants));
+		};
+
+		$scope.$watch("ParticipantInfo", function () {
+			for (var iIdx = 0; iIdx <= $scope.Participants.length - 1; iIdx++)
+			{
+				var oData = $scope.Participants[iIdx];
+
+				if (oData.Age > 80) {
+					oData.Color = "Red";
+				}
+				else
+				{
+					oData.Color = "White";
+				}
+			}
+		});
+
+		$scope.SaveParticipant = function () {
+			var oData = $scope.IsParticipantExists($scope.ParticipantInfo.PID);
+
+			if (oData.length <= 0) {
+				$scope.Participants.push($scope.ParticipantInfo);
+			}
+			else
+			{
+				oData[0].Name = $scope.ParticipantInfo.Name;
+				oData[0].Gender = $scope.ParticipantInfo.Gender;
+				oData[0].Age = $scope.ParticipantInfo.Age;
+			}
+			
+			$scope.ParticipantInfo = {
+				"PID": "",
+				"Name": "",
+				"Gender": "",
+				"Age": "",
+				"Color" : ""
+			};
+		}
+
+		$scope.FetchParticipant = function (pid) {
+			var oReturnData = {};
+
+			angular.forEach($scope.Participants, function (data) {
+
+				if (data.PID == pid) {
+					$scope.ParticipantInfo = {
+						"PID": data.PID,
+						"Name": data.Name,
+						"Gender": data.Gender,
+						"Age": data.Age
+					};
+
+					oReturnData = data;
+				}
+			})
+
+			return oReturnData;
+		};
+
+		$scope.FetchParticipantUsingFilterSvc = function (pid) {
+			var oData = $filter('filter')($scope.Participants, {PID : pid});
+
+			if (oData.length > 0) {
+				$scope.ParticipantInfo = {
+					"PID": oData[0].PID,
+					"Name": oData[0].Name,
+					"Gender": oData[0].Gender,
+					"Age": oData[0].Age
+				};
+
+			}
+
+			return oData;
+		};
+
+		$scope.IsParticipantExists = function (pid) {
+			return $filter('filter')($scope.Participants, { PID: pid });
+		};
+
+		$scope.GetParticipantInfoWebAPI = function (pid) {
+			return participantFactory.GetParticipantInfo(pid, function (data, status) {
+				if (status == 200) {
+					// Success
+				}
+				else
+				{
+					// Error Handling
+				}
+			});
+		};
+    }
+})();
---
+++ AngularJs/Scripts/app/services/participantFactory.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+		.module('AngularLab1')
+        .factory('participantFactory', participantFactory);
+
+    participantFactory.$inject = ['$log', '$http'];
+
+	function participantFactory($log, $http) {
+        var oParticipantService = {};
+
+		oParticipantService.GetTotalParticipants = function (oData) {
+			return "You have " + oData.length + " participants.";
+		};
+
+		oParticipantService.GetParticipantInfo = function (pid, callbackfunc) {
+			$http({ method: "GET", url: "/example/GetParticipantInfo/" + pid }).then(
+				function (response) {
+					callbackfunc(response.data, response.status);
+				}, function (error) {
+					$log.error("Error Occured - " + error.status);
+					callbackfunc(error.data, error.status);
+				}
+			);
+		};
+
+		return oParticipantService;		
+    }
+})();
